c8da23ce71bd4c33bb2986c9d082458363c0d324
Poramade Winyunawan <128454462+PrmWiny@users.noreply.github.com>
2023-04-19 23:31:41 +0700

    Add files via upload

---
+++ src/bytesstreams3/LAB1.java
@@ -0,0 +1,18 @@
+
+package bytesstreams3;
+
+
+public class LAB1 {
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        RedShapeDecorator redCircle = new RedShapeDecorator(circle);
+        redCircle.draw();
+        
+        Rectangle rectangle = new Rectangle();
+        RedShapeDecorator redRectangle = new RedShapeDecorator(rectangle);
+        redRectangle.draw();
+    
+    }
+    //
+}
